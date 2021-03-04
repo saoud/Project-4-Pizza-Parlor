23562b768be33abc4af539d4fb425054b7267ddd
Saoud <saoud@saoud.dev>
2021-03-04 02:19:06 -0800

    adding UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -22,3 +22,16 @@ function Pizza(size) {
       total += 14;
   }
 };
+
+$(document).ready(function() {
+  $("form#pizzaBuilder").submit(function(event) {
+    event.preventDefault();
+    let pizza = new Pizza($("#size").val())
+    $("input:checkbox[name=pizza-topping]:checked").each(function(){
+      let toppingString = $(this).val().toString();
+      const toppingArray = toppingString.split(",");
+      let newTopping = new Topping(toppingArray[0],parseFloat(toppingArray[1]));
+      pizza.addTopping(newTopping);      
+    });
+  });
+})
